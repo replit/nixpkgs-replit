bb9e877862f75a27faf729cddef8391fff2ef33e
Connor Brewster <cbrewster@hey.com>
2021-04-14 10:38:02 -0500

    build-prybar cleanup

--- pkgs/prybar/default.nix
+++ pkgs/prybar/default.nix
@@ -1,40 +1,40 @@
 { callPackage, python3, python2, lua5_1, readline, clojure, tcl }:
 let
-  buildPrybar = import ./buildPrybar.nix;
+  buildPrybar = attrs: callPackage(import ./build-prybar.nix attrs) {};
 in
 {
     # TODO: These don't quite work yet
 
     # Won't build
-    # prybar-R = callPackage (buildPrybar { language = "R"; buildInputs = [ pkgs.R ]; }) {};
+    # prybar-R = buildPrybar { language = "R"; buildInputs = [ pkgs.R ]; };
 
     # Julia 1.1 doesn't seem to be on nix
-    # prybar-julia = callPackage (buildPrybar { language = "julia"; buildInputs = [ julia_13 ]; }) {};
+    # prybar-julia = buildPrybar { language = "julia"; buildInputs = [ julia_13 ]; };
 
     # Has problem reading from prybar_assets. Seems like a bug with the impl.
-    # prybar-nodejs = callPackage (buildPrybar { language = "nodejs"; buildInputs = [ ]; }) {};
+    # prybar-nodejs = buildPrybar { language = "nodejs"; buildInputs = [ ]; };
 
     # Ruby 2.5 is deprecated!
-    # prybar-ruby = callPackage (buildPrybar { language = "ruby"; buildInputs = [ ruby_2_5 ]; }) {};
+    # prybar-ruby = buildPrybar { language = "ruby"; buildInputs = [ ruby_2_5 ]; };
 
     # Untested, spidermonkey not supported on darwin. Feel free to test on linux.
-    # prybar-spidermonkey = callPackage (buildPrybar { language = "spidermonkey"; buildInputs = [ spidermonkey ]; }) {};
+    # prybar-spidermonkey = buildPrybar { language = "spidermonkey"; buildInputs = [ spidermonkey ]; };
 
-    prybar-python2 = callPackage (buildPrybar { language = "python2"; buildInputs = [ python2 ]; }) {};
+    prybar-python2 = buildPrybar { language = "python2"; buildInputs = [ python2 ]; };
 
-    prybar-python3 = callPackage (buildPrybar { language = "python3"; buildInputs = [ python3 ]; }) {};
+    prybar-python3 = buildPrybar { language = "python3"; buildInputs = [ python3 ]; };
 
-    prybar-lua = callPackage (buildPrybar { language = "lua"; buildInputs = [ lua5_1 readline ]; }) {};
+    prybar-lua = buildPrybar { language = "lua"; buildInputs = [ lua5_1 readline ]; };
 
-    prybar-clojure = callPackage (buildPrybar { language = "clojure"; buildInputs = [ clojure ]; }) {};
+    prybar-clojure = buildPrybar { language = "clojure"; buildInputs = [ clojure ]; };
 
-    prybar-elisp = callPackage (buildPrybar { language = "elisp"; }) {};
+    prybar-elisp = buildPrybar { language = "elisp"; };
 
-    prybar-ocaml = callPackage (buildPrybar { language = "ocaml"; }) {};
+    prybar-ocaml = buildPrybar { language = "ocaml"; };
 
-    prybar-scala = callPackage (buildPrybar { language = "scala"; }) {};
+    prybar-scala = buildPrybar { language = "scala"; };
 
-    prybar-sqlite = callPackage (buildPrybar { language = "sqlite"; }) {};
+    prybar-sqlite = buildPrybar { language = "sqlite"; };
 
-    prybar-tcl = callPackage (buildPrybar { language = "tcl"; buildInputs = [ tcl ]; }) {};
+    prybar-tcl = buildPrybar { language = "tcl"; buildInputs = [ tcl ]; };
 }
