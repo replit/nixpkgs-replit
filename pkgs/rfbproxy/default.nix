d1b68384ed5af4313295e108bf8cdc48b82d5095
Connor Brewster <cbrewster@hey.com>
2021-04-13 21:00:46 -0500

    latest, pkg-config

--- pkgs/rfbproxy/default.nix
+++ pkgs/rfbproxy/default.nix
@@ -1,18 +1,19 @@
-{ lib, rustPlatform, fetchFromGitHub, openssl, stdenv, libpulseaudio }:
+{ lib, rustPlatform, fetchFromGitHub, openssl, stdenv, libpulseaudio, pkg-config }:
 
 rustPlatform.buildRustPackage rec {
   pname = "rfbproxy";
-  version = "cad8cb3";
+  version = "0.1.1-90b68e1";
 
   src = fetchFromGitHub {
     owner = "replit";
     repo = "rfbproxy";
-    rev = "${version}";
+    rev = "v${version}";
     sha256 = "0v33xmqv7fcaq1spiq7mvqpsx47r6ghgsbv4yhcghalxmy7cb2gc";
   };
 
   cargoSha256 = "17qnhkwmjggq6mbafy5jkdn6i8n6i7c09yq8c4w79vqh87f4y5dn";
 
   buildInputs = [ openssl libpulseaudio ];
+  nativeBuildInputs = [ pkg-config ];
 }
 
