0d567b48990e1b216961d42e78c4173018bea884
Connor Brewster <cbrewster@hey.com>
2021-12-07 13:47:28 -0600

    Add swift with build fixes

--- shell.nix
+++ shell.nix
@@ -1,3 +1,10 @@
 { pkgs ? import ./default.nix { } }:
 
-pkgs.mkShell { buildInputs = with pkgs; [ hello ]; }
+pkgs.mkShell {
+  buildInputs = with pkgs;
+    [
+      hello
+      # Swift takes a lot of compute to build, it is excluded from CI for now.
+      # swift
+    ];
+}
