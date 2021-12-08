2bd9daea5d9eb6260d5181def8230f34cc117dcf
Zach Anderson <zach.inbox@gmail.com>
2021-12-08 17:43:16 -0600

    Group overlay packages into single attribute

--- overlay.nix
+++ overlay.nix
@@ -2,13 +2,20 @@ self: super:
 
 with super.lib;
 
-rec {
-  hello = self.callPackage ./pkgs/hello { };
+let
+  override = rec {
+    # These packages will hide packages in the top level nixpkgs
+    hello = self.callPackage ./pkgs/hello { };
 
-  swift = self.callPackage ./pkgs/swift { };
-
-  replitPackages = {
-    jdt-language-server = self.callPackage ./pkgs/jdt-language-server { };
+    swift = self.callPackage ./pkgs/swift { };
   };
-}
+in {
+  replitPackages = rec {
+    # Any other packages should go in the replitPackages namespace
+    jdt-language-server = self.callPackage ./pkgs/jdt-language-server { };
+
+    # The override packages are injected into the replitPackages namespace as
+    # well so they can all be built together
+  } // override;
+} // override
 
