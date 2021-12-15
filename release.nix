d59ac6bb2b88af0e32219a1201deee9f414c421d
Connor Brewster <cbrewster@hey.com>
2021-12-15 13:16:57 -0600

    Fix release.nix

--- release.nix
+++ release.nix
@@ -4,9 +4,7 @@ let
 
   pkgs = import ./default.nix { };
 
-in {
-  pkgs.nodePackages.typescript-language-server
-  pkgs.nodePackages.yarn
-  pkgs.nodePackages.prettier
-  pkgs.nodePackages.svelte-language-server
+in
+{
+  inherit (pkgs.nodePackages) typescript-language-server yarn prettier svelte-language-server;
 }
