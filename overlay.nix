5cbcbe6fdc01d301c5e39c5a60ad72f379a1db90
Connor Brewster <cbrewster@hey.com>
2022-01-03 18:39:22 -0600

    Add Jest

--- overlay.nix
+++ overlay.nix
@@ -6,15 +6,16 @@ let
   override = {
     # These packages will hide packages in the top level nixpkgs
     swift = self.callPackage ./pkgs/swift { };
-
-    # Add our additional node packages
-    nodePackages = super.nodePackages // self.callPackage ./pkgs/node-packages { };
   };
 in {
   replitPackages = rec {
+    nodePackages = self.callPackage ./pkgs/node-packages { };
+
     # Any other packages should go in the replitPackages namespace
     jdt-language-server = self.callPackage ./pkgs/jdt-language-server { };
     replbox = self.callPackage ./pkgs/replbox { };
+    jest = nodePackages."jest-cli-23.6.0";
+    coffeescript = nodePackages.coffeescript;
 
     # The override packages are injected into the replitPackages namespace as
     # well so they can all be built together
