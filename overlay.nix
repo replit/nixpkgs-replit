2b91436b7324f8d21bbb25320fdb49b2ea35dcf9
Connor Brewster <cbrewster@hey.com>
2022-01-03 18:55:34 -0600

    Add Jest to stable 21.11 (#25)
    * Add Jest
    
    * Hand pick packages from nodePackages
    
    * Fix shell.nix

--- overlay.nix
+++ overlay.nix
@@ -3,18 +3,19 @@ self: super:
 with super.lib;
 
 let
+  nodePackages = self.callPackage ./pkgs/node-packages { };
+
   override = {
     # These packages will hide packages in the top level nixpkgs
     swift = self.callPackage ./pkgs/swift { };
-
-    # Add our additional node packages
-    nodePackages = super.nodePackages // self.callPackage ./pkgs/node-packages { };
   };
 in {
   replitPackages = rec {
     # Any other packages should go in the replitPackages namespace
     jdt-language-server = self.callPackage ./pkgs/jdt-language-server { };
     replbox = self.callPackage ./pkgs/replbox { };
+    jest = nodePackages."jest-cli-23.6.0";
+    coffeescript = nodePackages.coffeescript;
 
     # The override packages are injected into the replitPackages namespace as
     # well so they can all be built together
