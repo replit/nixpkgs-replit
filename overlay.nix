e696e461a907b7b485f1b1da8f200822d52f0121
Connor Brewster <cbrewster@hey.com>
2022-01-03 18:47:06 -0600

    Hand pick packages from nodePackages

--- overlay.nix
+++ overlay.nix
@@ -3,14 +3,14 @@ self: super:
 with super.lib;
 
 let
+  nodePackages = self.callPackage ./pkgs/node-packages { };
+
   override = {
     # These packages will hide packages in the top level nixpkgs
     swift = self.callPackage ./pkgs/swift { };
   };
 in {
   replitPackages = rec {
-    nodePackages = self.callPackage ./pkgs/node-packages { };
-
     # Any other packages should go in the replitPackages namespace
     jdt-language-server = self.callPackage ./pkgs/jdt-language-server { };
     replbox = self.callPackage ./pkgs/replbox { };
