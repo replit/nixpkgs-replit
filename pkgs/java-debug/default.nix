20229d956195a8163636eb0f2fb4a5be18d3691d
Zach Anderson <zach.inbox@gmail.com>
2022-02-15 12:52:27 -0600

    Patch python script with correct shebang

--- pkgs/java-debug/default.nix
+++ pkgs/java-debug/default.nix
@@ -2,6 +2,7 @@
 , callPackage
 , makeWrapper
 , jdt-language-server
+, python3
 }:
 let
   debug-plugin = callPackage ./debug-plugin.nix { };
@@ -12,11 +13,12 @@ in stdenv.mkDerivation {
   unpackPhase = "true";
   dontBuild = true;
 
-  #"-Dcom.microsoft.java.debug.serverAddress=localhost:0"
+  nativeBuildInputs = [ python3 ];
   buildInputs = [ makeWrapper ];
   installPhase = ''
     mkdir -p $out/bin
     cp ${./java-dap} $out/bin/java-dap
+    patchShebangs $out/bin/java-dap
 
     makeWrapper $out/bin/java-dap $out/bin/java-debug \
       --add-flags --use-ephemeral-port \
