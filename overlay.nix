536c4fb05768016ebae033851246db4594494b55
Toby Ho <toby@repl.it>
2022-12-01 13:35:42 -0500

    update

--- overlay.nix
+++ overlay.nix
@@ -34,6 +34,8 @@ rec {
     x11Support = true;
   };
 
+  jdt-language-server = self.callPackage ./pkgs/jdt-language-server { };
+
   replitPackages = {
     # Version string set when building overlay
     version = "GIT_SHA_HERE";
@@ -41,7 +43,8 @@ rec {
     # Any other packages should go in the replitPackages namespace
     replbox = self.callPackage ./pkgs/replbox { };
 
-    jdt-language-server = self.callPackage ./pkgs/jdt-language-server { };
+    inherit (self) jdt-language-server;
+    
     java-debug = self.callPackage ./pkgs/java-debug {
       inherit jdt-language-server;
       jdk = self.graalvm11-ce;
