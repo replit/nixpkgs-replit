6f948590dfb9c902e6cb1949e922d9fa14341525
Ryan Mulligan <ryan@ryantm.com>
2023-03-21 13:33:01 -0700

    allow passing system to default.nix

--- default.nix
+++ default.nix
@@ -1,10 +1,11 @@
 { sources ? import nix/sources.nix
 , channelName ? "nixpkgs-22.11"
 , channel ? sources.${channelName}
+, system ? "x86_64-linux"
 }:
 let
   overlay = (import ./overlay.nix) {
     inherit sources;
   };
 in
-import channel { overlays = [ overlay ]; }
+import channel { inherit system; overlays = [ overlay ]; }
