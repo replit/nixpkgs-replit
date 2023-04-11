d8290887269858478d6b66b20f5766d768c7ef94
Toby Ho <toby@repl.it>
2023-04-11 17:38:21 -0400

    updates for our fork of js-debug

--- pkgs/dapNode/default.nix
+++ pkgs/dapNode/default.nix
@@ -12,25 +12,34 @@ let
     version = "1.77.2";
 
     src = pkgs.fetchFromGitHub {
-      owner = "microsoft";
+      owner = "replit";
       repo = "vscode-js-debug";
-      rev = "483425e50cf0fb313610d2346b51459c8db556e2";
-      sha256 = "sha256-9YDSjDnUIVAa3n3VpUaqHIfYHCg4cBsQMN6Dz5r4EYk=";
+      rev = "5eac5f8255cd61b238547a7b383a00b971e223da";
+      sha256 = "sha256-XvK0r84GLwMZPhPm8IaF1+qVS/WFYkYhj2eqHqM9qlg=";
     };
 
     buildInputs = [ pkgs.nodejs ];
 
-    installPhase = ''
-    mkdir -p $out
+    installPhase = 
+    let nodeEscaped = builtins.replaceStrings ["/" "-" "."] ["\\/" "\\-" "\\."] pkgs.nodejs.outPath;
+    in
+    ''
+    mkdir -p $out/bin
     ln -s ${nodeDependencies}/lib/node_modules ./node_modules
     ln -s ${nodeDependencies}/lib/node_modules $out/node_modules
     ./node_modules/.bin/gulp
-    ./node_modules/.bin/gulp dapDebugServer:webpack-bundle
+    ./node_modules/.bin/gulp debugServerMain:webpack-bundle
 
     cp -r ./dist $out/dist
+
+    // Add shebang...
+    // Wrapping debugServerMain.js in a sh script doesn't work because it needs to read from
+    // a pipe directly from the parent (debugproxy)
+    printf "#!${nodeEscaped}\/bin\/node\n" | cat - $out/dist/src/debugServerMain.js > $out/dist/src/dap-node
+
+    chmod u+x $out/dist/src/dap-node
+    ln -s $out/dist/src/dap-node $out/bin/dap-node
     '';
   };
 in
-  pkgs.writeShellScriptBin "dap-node" ''
-    ${pkgs.nodejs}/bin/node ${js-debug}/dist/src/dapDebugServer.js
-  ''
+js-debug
