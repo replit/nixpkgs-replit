42ee93157c2652e4a132b452ce8e9adfdd5ac878
Colton Donnelly <colton@donn.io>
2023-04-13 23:45:40 +0000

    add qbasic module

--- overlay.nix
+++ overlay.nix
@@ -91,6 +91,7 @@ rec {
         java = mkModule ./modules/java.nix;
         nodejs = mkModule ./modules/nodejs.nix;
         php = mkModule ./modules/php.nix;
+        qbasic = mkModule ./modules/qbasic.nix;
         rust = mkModule ./modules/rust.nix;
         swift = mkModule ./modules/swift.nix;
         web = mkModule ./modules/web.nix;
