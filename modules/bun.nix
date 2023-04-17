d66ef9cf83ad29c7a655989d98087b79fa0d87d2
Colton Donnelly <colton@repl.it>
2023-04-17 14:25:41 -0700

    oops forgot to check in the module file

---
+++ modules/bun.nix
@@ -0,0 +1,39 @@
+{ pkgs, ... }:
+
+let
+  bun = pkgs.replitPackages.bun;
+
+  extensions = [".js" ".ts"];
+in
+
+{
+  name = "Bun Tools";
+  version = "1.0";
+
+  replit.runners.bun = {
+    name = "bun";
+    language = "bun";
+    inherit extensions;
+
+    start = "${bun}/bin/bun run $file";
+    fileParam = true;
+  };
+
+  replit.languageServers.ts-language-server = {
+    name = "TypeScript Language Server";
+    language = "bun";
+    inherit extensions;
+
+    start = "${pkgs.nodePackages.typescript-language-server}/bin/typescript-language-server --stdio";
+  };
+
+  replit.packagers.bun = {
+    name = "bun";
+    language = "bun";
+    features = {
+      packageSearch = true;
+      guessImports = true;
+      enabledForHosting = false;
+    };
+  };
+}
