2552a0a74c50161d39ed6c3166dd84625fd2de99
Ryan Mulligan <ryan@ryantm.com>
2023-04-21 10:08:35 -0700

    stop using import from derivation for replbox
    Why
    ===
    We think we were accidentally using import-from-derivation for the
    replbox derivation and this was causing nix-instantiation issues when
    references replbox on systems where it was not built.
    
    What changed
    ============
    Copied the files that should avoid IFD.
    
    Test plan
    =========
    Toby will test the nix-instantiate evaluations succeed

--- pkgs/replbox/default.nix
+++ pkgs/replbox/default.nix
@@ -11,4 +11,8 @@ mkYarnPackage rec {
     rev = "7f7873f2f2181ed0368aa6709218e06a4af3cc51";
     sha256 = "0bhm31rr9l2kgc44m6s2583h2ql3bg5dzham6zrxlmkfms82rg63";
   };
+
+  packageJSON = ./package.json;
+  yarnLock = ./yarn.lock;
+  yarnNix = ./yarn.nix;
 }
