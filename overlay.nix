8d055045a3b601c00426f0b98bd30022e923021f
Colton Donnelly <colton@donn.io>
2023-05-09 15:27:06 -0700

    don't build processing4 on nixpkgs-legacy

--- overlay.nix
+++ overlay.nix
@@ -132,7 +132,7 @@ rec {
         };
       });
 
-    processing4 = self.callPackage ./pkgs/processing4 { };
+    processing4 = if channelName != "nixpkgs-legacy" then self.callPackage ./pkgs/processing4 { } else null;
   };
 }
 
