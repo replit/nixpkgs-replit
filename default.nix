669ef300d0cdf9eaa93db568fa025dc5268dae68
Colton Donnelly <colton@donn.io>
2024-02-06 22:33:15 +0000

    add config attr to params

--- default.nix
+++ default.nix
@@ -2,10 +2,11 @@
 , channelName ? "nixpkgs-23.11"
 , channel ? sources.${channelName}
 , system ? "x86_64-linux"
+, config ? { }
 }:
 let
   overlay = (import ./overlay.nix) {
     inherit sources channelName;
   };
 in
-import channel { inherit system; overlays = [ overlay ]; }
+import channel { inherit config system; overlays = [ overlay ]; }
